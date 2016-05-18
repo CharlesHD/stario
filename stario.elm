c0ee5bb531ffaa86cd15c75918f21a10b314c2a8
charlesHD <charleshdespointes@gmail.com>
2016-05-18 22:55:04 +0200

    start stop create turn

--- stario.elm
+++ stario.elm
@@ -10,6 +10,9 @@ import Collage exposing (..)
 import Transform exposing (..)
 import Element exposing (..)
 import Color exposing (..)
+import Text exposing (..)
+import String
+import Array
 
 main =
   program { init = init,
@@ -17,8 +20,15 @@ main =
             update = update,
             subscriptions = subscriptions
           }
+-- UTIL
+either : Maybe a -> b -> (a -> b) -> b
+either m not f = case m of
+                   Nothing -> not
+                   Just v -> f v
 
-init = (Model 800 600 Dict.empty Dict.empty (CommandWindow "" []),  Cmd.none)
+
+-- MODEL
+init = (Model 600 450 Dict.empty Dict.empty (CommandWindow "" []),  Cmd.none)
 
 type alias Ship = {
     x : Float,
@@ -29,6 +39,7 @@ type alias Ship = {
     wantedRot : Float,
     powerOn : Bool
   }
+
 type alias Squadron = List String
 type alias CommandWindow = { input : String, messages : List String}
 
@@ -45,6 +56,57 @@ type Msg = Input String
          | Send
          | Tick Float
 
+addShip : String -> Model -> Model
+addShip name model =
+  let ship = { x = 0,
+               y = 0,
+               speed = 0.005,
+               rot = 0,
+               rotSpeed = 0.001,
+               wantedRot = 0,
+               powerOn = True
+             }
+  in log ("created a ship named " ++ name) {model | ships = Dict.insert name ship model.ships}
+
+moveShip : Float -> String -> Ship -> Ship
+moveShip t name ship =
+  let {x, y, speed, rot, rotSpeed, wantedRot, powerOn} = ship
+      potRot = rotSpeed * t
+      rotShip = if abs (wantedRot - rot) <= potRot
+                then {ship | rot = wantedRot }
+                else if wantedRot > rot
+                     then {ship | rot = rot + potRot}
+                     else {ship | rot = rot - potRot}
+      dep = speed * t
+  in if powerOn
+     then {rotShip | x = x + dep * cos ship.rot, y = y + dep * sin ship.rot}
+     else rotShip
+
+updateShip : (Ship -> Ship) -> String -> Model -> Model
+updateShip f name model =  if Dict.member name model.ships
+                           then {model | ships = Dict.update name (Maybe.map f) model.ships}
+                           else log ("there is no ship named" ++ name) model
+
+turnShip : Float -> Ship -> Ship
+turnShip deg ship = {ship | wantedRot = ship.wantedRot + deg}
+
+stopShip : Ship -> Ship
+stopShip ship = {ship | powerOn = False}
+
+startShip : Ship -> Ship
+startShip ship = {ship | powerOn = True}
+
+log : String -> Model -> Model
+log msg model = let cmds = model.cmds
+                    ncmds = {cmds | messages = msg :: cmds.messages}
+                in {model | cmds = ncmds}
+
+clearInput : Model -> Model
+clearInput model = let cmds = model.cmds
+                       ncmds = {cmds | input = ""}
+                   in {model | cmds = ncmds}
+
+-- UPDATE
 update msg model =
   case msg of
     Input newInput ->
@@ -59,40 +121,41 @@ gameTick t model
          = {model | ships = Dict.map (moveShip t) model.ships}
 
 parseCommand model =
-  let name = model.cmds.input
-  in addShip name model
+  let cmdargs = String.words model.cmds.input
+      nmodel = clearInput model
+      cmdMaybe = List.head cmdargs
+      args = Maybe.map Array.fromList <| List.tail cmdargs
+  in case cmdMaybe of
+       Nothing -> nmodel
+       Just cmd -> case cmd of
+                     "create" -> either (Maybe.andThen args (Array.get 0))
+                                 (log "create take a name as parameter" nmodel)
+                                 (\x -> addShip (String.trim x) nmodel)
+                     "turn" -> let name = Maybe.andThen args (Array.get 0)
+                                   sdeg = Maybe.andThen args (Array.get 1)
+                                   deg = Maybe.andThen sdeg (Result.toMaybe << String.toFloat)
+                                   pair = Maybe.map2 (\x y -> (x,y)) name deg
+                               in either pair
+                                    (log ("Turn take a name and a degree") nmodel)
+                                    (\ (n,r) -> updateShip (turnShip (degrees r)) n (log ("turning " ++ n ++ " by " ++ toString r ++ " degrees") nmodel))
+                     "stop" -> either (Maybe.andThen args (Array.get 0))
+                                      (log "stop take a ship name as a parameter" nmodel)
+                                      (\x -> updateShip stopShip x (log ("stopped " ++ x) nmodel))
+                     "start" -> either (Maybe.andThen args (Array.get 0))
+                                      (log "start take a ship name as a parameter" nmodel)
+                                      (\x -> updateShip startShip x (log ("started " ++ x) nmodel))
 
-addShip : String -> Model -> Model
-addShip name model =
-  let ship = { x = 0,
-               y = 0,
-               speed = 10,
-               rot = 0,
-               rotSpeed = 1,
-               wantedRot = 0,
-               powerOn = True
-             }
-  in {model | ships = Dict.insert name ship model.ships}
 
-moveShip : Float -> String -> Ship -> Ship
-moveShip t name ship =
-  let {x, y, speed, rot, rotSpeed, wantedRot, powerOn} = ship
-      potRot = rotSpeed * t
-      rotShip = if abs (wantedRot - rot) <= potRot
-                then ship
-                else if wantedRot > rot
-                     then {ship | rot = rot + potRot}
-                     else {ship | rot = rot - potRot}
-      dep = speed * t
-  in if powerOn
-     then {rotShip | x = x + cos ship.rot, y = y + sin ship.rot}
-     else rotShip
+                     otherwise -> log ("Unknown command : " ++ cmd) nmodel
 
+-- SUBSCRIPTIONS
 
 subscriptions model = AnimationFrame.diffs Tick
 
+-- VIEWS
+
 view model =
-  let ships = List.map viewShip (Dict.values model.ships)
+  let ships = Dict.values <| Dict.map viewShip model.ships
       background = rect model.width model.height |> filled black
       elems = background :: ships
       (w', h') = (round model.width, round model.height)
@@ -100,17 +163,22 @@ view model =
          [ collage w' h' elems |> toHtml
          , input [onInput Input, value model.cmds.input] []
          , button [onClick Send] [Html.text "Send"]
-         , div [] (List.map viewMessage (List.reverse model.cmds.messages))
+         , div [] <| List.map viewMessage (List.reverse <| List.take 5 model.cmds.messages)
+         -- , div [] <| List.map (viewMessage << toString) (Dict.toList model.ships)
          ]
 
 viewMessage msg =
   div [] [ Html.text msg ]
 
-viewShip : Ship -> Form
-viewShip ship =
+
+viewShip : String -> Ship -> Form
+viewShip name ship =
   let shipgon = ngon 3 10
-      shipform = filled green shipgon
+      nametext =move (-10, -10) <| Collage.text <| Text.height 20 <| Text.color white (fromString name)
+      shipform = filled red shipgon
       translat = translation ship.x ship.y
       rot = rotation ship.rot
       transfo = multiply translat rot
-  in groupTransform transfo [shipform]
+  in groupTransform transfo [ shipform
+                            , nametext
+                            ]
